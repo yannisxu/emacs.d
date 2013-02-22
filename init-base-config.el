430db8d89095485df598bcade41fc838fc4c91d4
unknown <yannisxu@021ZJ1792.synacast.local>
2013-02-22 22:12:17 +0800

    config my emacs

---
+++ init-base-config.el
@@ -0,0 +1,43 @@
+;;关闭错误提示音
+(setq visible-bell t)
+
+;;显示行号
+(setq column-number-mode t)
+
+;;设置fill-column设为60,让阅读更加友好
+(setq default-fill-column 60)
+
+;;标题栏中显示buffer名字
+(setq frame-title-format "emacs@%b")
+
+;;匹配括号时显示另外一边的括号，而不是跳转
+(show-paren-mode t)
+(setq show-paren-style 'parentheses)
+
+;;识别中文标点,不用在fill时在句号后插入两个空格
+(setq sentence-end "\\([。！？]\\|……\\|[.?!][]\"')}]*\\($\\|[ \t]\\)\\)[ \t\n]*")
+(setq sentence-end-double-space nil)
+
+;;允许使用递归minibuffer
+(setq enable-recursive-minibuffers t)
+
+
+;;光标靠近鼠标时，让鼠标自动让开
+(mouse-avoidance-mode 'animate)
+
+;;允许Emacs直接打开和显示图片
+(auto-image-file-mode)
+
+;;语法加亮
+(global-font-lock-mode t)
+
+;;设置个人信息
+(setq user-full-name "Yannis Xu")
+(setq user-mail-address "excellentbright@gmail.com")
+
+;;打开缺省功能
+(put 'set-goal-column 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
+(put 'LaTeX-hide-environment 'disabled nil)
--- init.el
+++ init.el
@@ -1,6 +1,4 @@
-;;; This file bootstraps the configuration, which is divided into
-;;; a number of other files.
-
+;;Load_path
 (add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 
 ;;----------------------------------------------------------------------------
@@ -11,99 +9,23 @@
 (defconst *is-carbon-emacs* (eq window-system 'mac))
 (defconst *is-cocoa-emacs* (and *is-a-mac* (eq window-system 'ns)))
 
-;;----------------------------------------------------------------------------
-;; Load configs for specific features and modes
-;;----------------------------------------------------------------------------
-(require 'init-compat)
-(require 'init-utils)
-(require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
-(require 'init-elpa)      ;; Install/load required packages
-(require 'init-exec-path) ;; Set up $PATH
-(require 'init-frame-hooks)
-(require 'init-xterm)
-(require 'init-themes)
-(require 'init-osx-keys)
-(require 'init-gui-frames)
-(require 'init-maxframe)
-(require 'init-proxies)
-(require 'init-dired)
-(require 'init-isearch)
-(require 'init-uniquify)
-(require 'init-ibuffer)
-(require 'init-flymake)
+;;添加安装源  
+;;M-x eval-buffer to evaluate it, and then do M-x package-refresh-contents to load in the package listing.
+(require 'package)
+(add-to-list 'package-archives 
+    '("marmalade" .
+      "http://marmalade-repo.org/packages/"))
+(package-initialize)
 
-(require 'init-recentf)
-(require 'init-ido)
-(require 'init-hippie-expand)
+;;基本配置
+
+
+;;绑定快捷键
+(require 'init-keybind)
+
+;;elpa安装包
+(require 'init-elpa)
+
+;;自动补全
 (require 'init-auto-complete)
-(require 'init-windows)
-(require 'init-sessions)
-(require 'init-fonts)
-(require 'init-mmm)
-(require 'init-growl)
 
-(require 'init-editing-utils)
-
-(require 'init-darcs)
-(require 'init-git)
-
-(require 'init-crontab)
-(require 'init-textile)
-(require 'init-markdown)
-(require 'init-csv)
-(require 'init-erlang)
-(require 'init-javascript)
-(require 'init-sh)
-(require 'init-php)
-(require 'init-org)
-(require 'init-nxml)
-(require 'init-css)
-(require 'init-haml)
-(require 'init-python-mode)
-(require 'init-haskell)
-(require 'init-ruby-mode)
-(require 'init-rails)
-
-(require 'init-lisp)
-(require 'init-slime)
-(require 'init-clojure)
-(require 'init-common-lisp)
-
-(when *spell-check-support-enabled*
-  (require 'init-spelling))
-
-(require 'init-marmalade)
-(require 'init-misc)
-
-
-;;----------------------------------------------------------------------------
-;; Allow access from emacsclient
-;;----------------------------------------------------------------------------
-(require 'server)
-(unless (server-running-p)
-  (server-start))
-
-
-;;----------------------------------------------------------------------------
-;; Variables configured via the interactive 'customize' interface
-;;----------------------------------------------------------------------------
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
-
-;;----------------------------------------------------------------------------
-;; Allow users to provide an optional "init-local" containing personal settings
-;;----------------------------------------------------------------------------
-(require 'init-local nil t)
-
-
-;;----------------------------------------------------------------------------
-;; Locales (setting them earlier in this file doesn't work in X)
-;;----------------------------------------------------------------------------
-(require 'init-locales)
-
-
-;; Local Variables:
-;; coding: utf-8
-;; no-byte-compile: t
-;; End:
