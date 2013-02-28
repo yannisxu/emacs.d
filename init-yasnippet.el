6771db8940dab75ae12cf58db4921e2574843038
yannisxu <excellentbright@gmail.com>
2013-02-28 11:00:41 +0800

    rm unicad.el

---
+++ init-yasnippet.el
@@ -0,0 +1,14 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;; -*- Mode: Emacs-Lisp -*- ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Filename: init-yasnippet.el
+;; Description: 
+;; Author: Yannis Xu
+;; Created: 周四 二月 28 10:48:50 2013 (+0800)
+;; Version: 
+;; Last-Updated: 周四 二月 28 10:57:59 2013 (+0800)
+;;           By: Yannis Xu
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-yasnippet.el ends here
+
+(require 'yasnippet)
+;;(setq yas-snippet-dirs '("~/.emacs.d/elpa/"))
